3207579180c522443a10de59f1d8c6324a4bc259
soyjack <81850273+soyjack@users.noreply.github.com>
2021-04-21 15:42:53 +0300

    Create Connector.java
    A connector to MySql driver

---
+++ LMS/src/Connector.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+import javax.swing.*;
+
+
+public class Connector {
+	Connection conn = null;
+	static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+	static final String DATABASE_URL = "jdbc:mysql://localhost/Library";
+	public static Connection dbconnection() {
+		try {
+			Class.forName(DRIVER);
+			Connection conn = DriverManager.getConnection(DATABASE_URL,"root","letmeinside");
+			return conn;
+		}catch(Exception e) {
+			JOptionPane.showMessageDialog(null, e);
+			return null;
+		}
+	}
+}
